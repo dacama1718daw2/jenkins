dd23e775bccdbbd4be8e4b1da41dd5532c647c4a
dacama1718daw2 <daw2@daw2m08.fjeclot.net>
2018-02-01 16:51:09 +0100

    Modificació  1 de la prova d'IC amb Jenkins

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -27,6 +27,7 @@ public class Jenkins {
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
             bw.close();
+            bw.write(" Modificació del projecte jenkins");bw.newLine();
         }
     }
 }
